499947f726b830966cf99ae81bbcebc6349e95d6
승엽 <137405141+SEUNGBOONG@users.noreply.github.com>
2024-05-05 13:57:48 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,11 @@
+class Solution {
+   public int solution(int n, int k) {
+        int answer = 0;
+        int serviceN=n/10;
+        int sheep=12000;
+        int coke=2000;
+        
+        answer=sheep*n+coke*k-coke*serviceN;
+        return answer;
+    }
+}
