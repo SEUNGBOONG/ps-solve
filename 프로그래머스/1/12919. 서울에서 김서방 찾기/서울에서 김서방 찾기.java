ec45c98319f5d0f65efbecb6492c6f4d1d174ed0
승엽 <137405141+SEUNGBOONG@users.noreply.github.com>
2024-05-12 18:32:21 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 1.00 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String[] seoul) {
+        int i=0;
+        for(String s: seoul){
+            if(s.equals("Kim")){
+                break;
+            }
+            else
+                i+=1;
+        }
+        return String.format("김서방은 %d에 있다", i);
+    }
+}
