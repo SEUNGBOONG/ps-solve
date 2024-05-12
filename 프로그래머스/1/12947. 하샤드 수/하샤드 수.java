b86f254e95d9bcd5673d508b82417f17269c67d1
승엽 <137405141+SEUNGBOONG@users.noreply.github.com>
2024-05-12 17:45:15 +0900

    [level 1] Title: 하샤드 수, Time: 0.16 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum=0;
+        boolean answer = false;
+        String str = String.valueOf(x);
+        String[] numArr=str.split("");
+        for(String num:numArr){
+            sum+=Integer.parseInt(num);
+        }
+        if(x%sum==0){
+            answer=true;
+        }
+        return answer;
+    }
+
+}
