faa12766c0974d6a96c8a797c5692df059f6561e
승엽 <137405141+SEUNGBOONG@users.noreply.github.com>
2024-06-21 16:42:17 +0900

    [level 1] Title: 과일 장수, Time: 0.43 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+class Solution {
+  public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        List<Integer> list = new ArrayList<>();
+        for(int s : score){
+            list.add(s);
+        }
+        Collections.sort(list);
+        for(int i = score.length -m; i >= 0; i-=m){
+            answer += m*(list.get(i));
+        }
+        return answer;
+    }
+
+}
